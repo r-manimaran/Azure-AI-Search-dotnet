473c0a0446364793bebe55a08cd00511e1d44ebe
Manimaran <r.manimaran@yahoo.co.in>
2024-07-18 23:02:25 -0400

    initial commit

---
+++ Azure.AI.Search.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.AI.Search.ConsoleApp;
+
+System.Console.WriteLine("Starting Azure AI Search Tool");
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("Options:");
+Console.WriteLine("1. Create Index");
+Console.WriteLine("2. Upload Documents");
+Console.WriteLine("3. Search Documents");
+Console.WriteLine("4. Exit");
+Console.Write("Enter your choice: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string choice = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+Console.WriteLine("-----------------------------------");
+switch (choice)
+{
+    case "1":
+        await SearchHelper.CreateIndex();
+        break;
+    case "2":
+        await SearchHelper.UploadDocuments();
+        break;
+    case "3":
+        await SearchHelper.SearchDocuments();
+        break;
+    case "4":
+        return;
+    default:
+        Console.WriteLine("Invalid choice. Please try again.");
+        break;
+}
---
+++ Azure.AI.Search.ConsoleApp/SearchHelper.cs
@@ -0,0 +1,32 @@
+namespace Azure.AI.Search.ConsoleApp;
+/// <summary>
+/// Helper class for search operations.
+/// </summary>
+public static class SearchHelper
+{
+    /// <summary>
+    /// Create Azure AI Search Index
+    /// </summary>
+    public static async Task CreateIndex()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// Upload the Documents to the newly Created Index
+    /// </summary>
+    public static async Task UploadDocuments()
+    {
+
+    }
+
+    /// <summary>
+    /// Search the Documents from the Index
+    /// </summary>
+    public static async Task SearchDocuments()
+    {
+
+    }
+   
+}
